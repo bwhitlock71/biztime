3ab91a50c19b4a7e1862131ce9e6b50558ba0a59
Brayton Whitlock <brayton94@outlook.com>
2023-09-04 15:25:13 -0700

    updates

--- routes/companies.js
+++ routes/companies.js
@@ -27,7 +27,7 @@ router.get("/:code", async (req, res, next)=>{
     }
 })
 
-router.post("/", async function(req, res, next){
+router.post("/", async (req, res, next) =>{
     try{
     let { code, name, description } = req.body;
     const results = await db.query('INSERT INTO companies (code, name, description) VALUES ($1, $2, $3) RETURNING code, name, description', [code, name, description]);
@@ -38,5 +38,41 @@ router.post("/", async function(req, res, next){
         return next(e);
     }
 })
+
+router.put("/:code", async (req, res, next) =>{
+    try{
+       let { name, description } = req.body;
+       let code = req.params.code;
+       const results = await db.query(
+        `UPDATE companies SET name=$1, description=$2 WHERE code=$3
+        RETURNING code, name, description`,
+        [name, description, code]);
+        
+    if ( results.rows.length === 0) {
+        throw new ExpressError(`Company does not exist`, 404)
+    } else {
+        return res.json({"company": results.rows[0]});
+        }
+    } catch(e){
+        return next(e);
+    }
+});
+
+router.delete("/:code", async (req, res, next) =>{
+    try{
+        let code = req.params.code;
+
+        const results = await db.query(
+            `DELETE FROM companies WHERE code=$1 RETURNING code`, [code]);
+
+        if (results.rows.length === 0) {
+            throw new ExpressError(`Company does not exist`, 404)
+        } else {
+            return res.json({"status": "deleted"});
+        }
+    } catch(e){
+        return next(e);
+    }
+    });
 
 module.exports = router;
